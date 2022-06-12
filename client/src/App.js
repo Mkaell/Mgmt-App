05c105ae15c3bcc5a23b0b356c6f5cced05b1a24
Mkaell <mrohacevich@yandex.ru>
2022-06-12 13:57:11 +0300

    added project functionality

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,8 @@ import Header from "./components/Header";
 import { ApolloProvider, ApolloClient, InMemoryCache } from "@apollo/client";
 import Clients from "./components/Clients";
 import AddClientModal from "./components/AddClientModal";
+import AddProjectModal from "./components/AddProjectModal";
+import Projects from "./components/Projects";
 
 const cache = new InMemoryCache({
     typePolicies: {
@@ -34,6 +36,8 @@ function App() {
                 <Header />
                 <div className="container">
                     <AddClientModal />
+                    <AddProjectModal />
+                    <Projects />
                     <Clients />
                 </div>
             </ApolloProvider>
